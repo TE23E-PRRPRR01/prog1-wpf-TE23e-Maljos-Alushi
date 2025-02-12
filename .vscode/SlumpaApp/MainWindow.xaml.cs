978b39aea8cbab4732591ea69af95a13f9ea1321
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2025-02-12 15:02:35 +0100

    tränat på xaml och trypars

---
+++ .vscode/SlumpaApp/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SlumpaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void SlumpTal (object sender, RoutedEventArgs e)
+    {
+        // läsa av maxvärde
+        //int max = int.Parse(txbMax.Text) + 1;
+
+        // Läsa av utan risk för krasch
+        if (!int.TryParse(txbMax.Text , out int max))
+        {
+            txbRetur.Text = "Fel skriv nummer ".ToString();
+            return;
+        }
+
+        // slumpa ett tal 1-100
+        int slumptal = Random.Shared.Next(1, max);
+
+        // Skriv ut sluptalet i textboxen
+         txbSlump.Text = $"{slumptal}";
+    }
+}
