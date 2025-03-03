8f5dbce98d84b5a7a1a7e1f5c347b0cebc91436f
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2025-03-03 14:53:48 +0100

    Email application

---
+++ .vscode/EpostApp/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Mail;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EpostApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+    private void KlickSkicka(object sender, RoutedEventArgs e)
+    {
+         string epost = tbxEpost.Text;
+         string meddelande = tbxMeddelande.Text;
+
+         SmtpClient smtp = new SmtpClient("smtp.gmail.com",587);
+         smtp.EnableSsl = true;
+         smtp.Credentials = new NetworkCredential("user", "pass");
+
+         if (epost != "" & meddelande != "")
+         {
+            smtp.Send("bumba@gmail.com", epost, "Epost från appen", meddelande);
+            lblStatus.Content = "Ok";
+            
+         }
+         else
+         {
+         lblStatus.Content = "Fel! Något av fälten är tomma.";
+            
+         }
+
+
+       
+    }
+}
