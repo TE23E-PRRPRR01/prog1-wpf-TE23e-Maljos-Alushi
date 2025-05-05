0354eeb1f043ef549e819ca51c03135579eb96ce
Maljos Alushi <alushi.maljos@elev.ga.ntig.se.>
2025-05-05 13:50:26 +0200

    valuta app klar

---
+++ .vscode/ValutaApp/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValutaApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    double Usd = (10.5);
+    double Eur = (11.2);
+    double Gbp = (13.5);
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+
+    private void KlickUsd(object sender, RoutedEventArgs e)
+    {
+        if(double.TryParse(txbBelopp.Text, out double belopp))
+          {
+            txbResultat.Text = (belopp / Usd).ToString("0.00");
+          }
+          else
+          {
+              txbResultat.Text = "Ange giltig belopp";
+          }
+    }
+    
+    private void KlickEur(object sender, RoutedEventArgs e)
+    {
+    if(double.TryParse(txbBelopp.Text, out double belopp))
+          {
+            txbResultat.Text = (belopp / Eur).ToString("0.00");
+          }
+          else{
+              txbResultat.Text = "Ange giltig belopp"; 
+              }
+    }
+    
+    private void KlickGbp(object sender, RoutedEventArgs e)
+    {
+       if(double.TryParse(txbBelopp.Text, out double belopp))
+          {
+           txbResultat.Text = (belopp / Gbp).ToString("0.00");
+          }
+          else
+          {
+              txbResultat.Text = "Ange giltig belopp"; 
+          }
+    }
+}
